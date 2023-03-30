8acfa3828627fc3155a35f36532dec531acaaee1
Quadsam <sam@quadsam.com>
2023-03-30 01:11:54 -0600

    Minified

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-
-if ! autoreconf --install --verbose; then
-	printf 'Unable to find autoreconf from GNU autotools!\n'
-	printf 'Please install autotools and try again.\n'
-	exit 1
+if ! autoreconf --install --verbose;then
+printf 'Unable to find autoreconf from GNU autotools!\n'
+printf 'Please install autotools and try again.\n'
+exit 1
 fi
