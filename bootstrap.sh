5ebb53feab7993396f74f4348c9aaa433303ee92
Quadsam <sam@quadsam.com>
2023-03-30 00:43:22 -0600

    Added bootstrap script for simplified building

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if ! autoreconf --install --verbose; then
+	printf 'Unable to find autoreconf from GNU autotools!\n'
+	printf 'Please install autotools and try again.\n'
+	exit 1
+fi
